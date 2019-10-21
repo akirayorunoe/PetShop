312dbfdfd5511e80f0d3a60a6da6e8d3439994e6
Hoang Long <cuncongame@gmail.com>
2019-10-21 12:52:20 +0700

    Test

--- PetShop/App.js
+++ PetShop/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import TNav from './navigator/TabNav';
 //Add nav as component here
+//Hello World
 class App extends Component {
   render() {
     return <TNav />;
