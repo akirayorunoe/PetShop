f908ca82ccfa362e0dece0bddc2e0445fe994d76
= <=>
2019-10-21 21:34:55 +0700

    Edit: Stack Nav, Login, Sign up, Forget Pass

--- PetShop/navigator/StackNav.js
+++ PetShop/navigator/StackNav.js
@@ -2,6 +2,7 @@ import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
 import Login from '../screens/Login';
 import SignUp from '../screens/SignUp';
+import ForgetPass from '../screens/ForgetPass';
 
 const stackNav = createStackNavigator(
   {
@@ -13,6 +14,13 @@ const stackNav = createStackNavigator(
     },
     SignUp: {
       screen: SignUp,
+      navigationOptions: () => ({
+        //the header will not have a background
+        headerTransparent: true,
+      }),
+    },
+    ForgetPass: {
+      screen: ForgetPass,
       navigationOptions: () => ({
         //the header will not have a background
         headerTransparent: true,
--- PetShop/screens/ForgetPass.js
+++ PetShop/screens/ForgetPass.js
@@ -1,12 +1,38 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
-
+import {View, StyleSheet, Text} from 'react-native';
+import Header from '../components/Form/Header';
+import Input from '../components/Form/Input';
+import Button from '../components/Form/Button';
 export default class ForgetPass extends Component {
   render() {
-    return <View style={styles.container}></View>;
+    return (
+      <View style={styles.container}>
+        <View style={styles.child}>
+          <Header setText="FORGOT  PASSWORD" />
+          <Text
+            style={{
+              top: 50,
+              fontFamily: 'LobsterTwo-Regular',
+              fontSize: 20,
+              paddingHorizontal: 20,
+            }}>
+            Please, enter your email address. You will receive a link to create
+            a new password via email.
+          </Text>
+          <View style={{top: 20}}>
+            <Input placeholder="Email" />
+            <Button setText="Send" />
+          </View>
+        </View>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {flex: 1, backgroundColor: '#F5F7FA'},
+  child: {
+    alignItems: 'center',
+    top: 30,
+  },
 });
